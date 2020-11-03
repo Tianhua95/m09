2325202226a382d88b97864a4769a7bef658fb24
Huang Tianhua <huangtianhua@HuangdeMacBook-Pro.local>
2020-11-03 13:19:48 +0800

    m09

---
+++ parts/meta.php
@@ -0,0 +1,8 @@
+	<meta name="viewport" content="width=device-width">
+
+	<base href="http://www.tianhuahuang.net/aau/wnm608/huang.tianhua/">
+
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+    <link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="css/storetheme.css">
+	
---
+++ parts/navbar.php
@@ -0,0 +1,18 @@
+	<header class="navbar">
+		<div class="container display-flex">
+				<div class="flex-none">
+					<h1>store</h1>
+
+				</div>
+				<div class="flex-streth"></div>
+				<nav class="nav nav-flex flex-none">
+					
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="product_list.php">store</a></li>
+						<li><a href="#">Link 3</a></li>
+					</ul>
+				</nav>
+		</div>
+
+	</header>
